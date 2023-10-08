861e9c9c62f259075b04bea329b3ea51dc458410
Ryan <ryanchien8125@gmail.com>
2023-10-08 16:49:01 +0800

    Nuxt I18n 模組基本使用方法

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  devtools: { enabled: true }
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+  devtools: { enabled: true },
+  i18n: {
+    langDir: 'locales',
+    locales: [
+      {
+        code: 'en',
+        iso: 'en-US',
+        file: 'en.json'
+      },
+      {
+        code: 'zh-tw',
+        iso: 'zh-TW',
+        file: 'zh-tw.json'
+      }
+    ],
+    defaultLocale: 'zh-tw',
+    strategy: 'prefix_and_default',
+    detectBrowserLanguage: {
+      redirectOn: 'root'
+    }
+  }
 })
---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col items-center bg-white">
+    <p class="my-8 text-4xl font-semibold text-rose-500">{{ $router.currentRoute.value.href }}</p>
+    <h1 class="text-6xl font-medium text-blue-500">
+      {{ $t('about') }}
+    </h1>
+  </div>
+</template>
